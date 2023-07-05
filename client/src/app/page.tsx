c801f3e4a0385d850e9d9d938ce3b673e8d387f0
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-07-05 13:28:58 -0300

    .

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -53,46 +53,47 @@ export default function Home() {
         <div className="ml-[231.500px] mr-[231.500px] pb-8 pl-3 pr-3 pt-8">
           <div className="">
             <div>
-              <h1 className="mb-6 text-7xl font-semibold text-white pl-[6px] pt-[48px] pr-[12px]">
+              .{' '}
+              <h1 className="mb-6 pl-[6px] pr-[12px] pt-[48px] text-7xl font-semibold text-white">
                 Inter Day 2023
               </h1>
             </div>
 
-            <div className=" pr-[12px] pl-[6px] mb-4">
+            <div className=" mb-4 pl-[6px] pr-[12px]">
               <p className=" mb-4 text-left text-xl leading-[25px] text-white">
                 Um dia de Black Friday no meio de julho? <br /> O Inter Shop
                 tem!
               </p>
-              <span className=" font-bold text-[17px] text-white ">
+              <span className=" text-[17px] font-bold text-white ">
                 Faltam:
               </span>
             </div>
 
             <div>
               <div className=" flex ">
-                <div className="flex flex-col w-[87px] h-[87px] bg-white rounded-[8px] items-center justify-center">
-                  <span className=" text-[32px] leading-[32px] font-bold">
+                <div className="flex h-[87px] w-[87px] flex-col items-center justify-center rounded-[8px] bg-white">
+                  <span className=" text-[32px] font-bold leading-[32px]">
                     2
                   </span>
                   <span>dias</span>
                 </div>
 
-                <div className="flex flex-col w-[87px] h-[87px] bg-white rounded-[8px] items-center justify-center ml-6">
-                  <span className=" text-[32px] leading-[32px] font-bold">
+                <div className="ml-6 flex h-[87px] w-[87px] flex-col items-center justify-center rounded-[8px] bg-white">
+                  <span className=" text-[32px] font-bold leading-[32px]">
                     2
                   </span>
                   <span>horas</span>
                 </div>
 
-                <div className="flex flex-col w-[87px] h-[87px] bg-white rounded-[8px] items-center justify-center ml-6">
-                  <span className=" text-[32px] leading-[32px] font-bold">
+                <div className="ml-6 flex h-[87px] w-[87px] flex-col items-center justify-center rounded-[8px] bg-white">
+                  <span className=" text-[32px] font-bold leading-[32px]">
                     2
                   </span>
                   <span>minutos</span>
                 </div>
 
-                <div className="flex flex-col w-[87px] h-[87px] bg-white rounded-[8px] items-center justify-center ml-6">
-                  <span className=" text-[32px] leading-[32px] font-bold">
+                <div className="ml-6 flex h-[87px] w-[87px] flex-col items-center justify-center rounded-[8px] bg-white">
+                  <span className=" text-[32px] font-bold leading-[32px]">
                     2
                   </span>
                   <span>segundos</span>
@@ -101,7 +102,7 @@ export default function Home() {
               <div>
                 <a
                   href=""
-                  className=" flex  items-center  justify-center text-[14px] leading-[17px] font-bold rounded-[8px] pt-[6px] pb-[7px] px-[20px] border-white bg-white mt-6 text-[#161616] w-[420px] h-12  "
+                  className=" mt-6  flex  h-12 w-[420px] items-center justify-center rounded-[8px] border-white bg-white px-[20px] pb-[7px] pt-[6px] text-[14px] font-bold leading-[17px] text-[#161616]  "
                 >
                   Saiba Mais
                 </a>
