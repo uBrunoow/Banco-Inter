0e0b0f8f7c7221cf3c1af59926ef78ffc992d809
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-07-07 21:12:29 -0300

    countdown

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,73 +1,13 @@
 import Header from "@/components/header";
 import Popup from "@/components/popup";
+import Main from "@/components/main"
 
 
 export default function Home() {
   return (
     <body>
       <Header/>
-      <section className="flex h-[820px] items-center bg-[url('../assets/background.png')] bg-cover bg-right-center bg-no-repeat ">
-        <div className="ml-[231.500px] mr-[231.500px] pb-8 pl-3 pr-3 pt-8">
-          <div className="">
-            <div>
-              .{" "}
-              <h1 className="mb-6 pl-[6px] pr-[12px] pt-[48px] text-7xl font-semibold text-white font-sora ">
-                Inter Day 2023
-              </h1>
-            </div>
-
-            <div className=" mb-4 pl-[6px] pr-[12px]">
-              <p className=" mb-4 text-left text-xl leading-[25px] text-white">
-                Um dia de Black Friday no meio de julho? <br /> O Inter Shop
-                tem!
-              </p>
-              <span className=" text-[17px] font-bold text-white ">
-                Faltam:
-              </span>
-            </div>
-
-            <div>
-              <div className=" flex ">
-                <div className="flex h-[87px] w-[87px] flex-col items-center justify-center rounded-[8px] bg-[#854000]">
-                  <span className=" text-[32px] font-bold leading-[40px] text-white">
-                    2
-                  </span>
-                  <span className=" font-bold text-[12px] leading-4 text-white">dias</span>
-                </div>
-
-                <div className="ml-6 flex h-[87px] w-[87px] flex-col items-center justify-center rounded-[8px] bg-[#854000]">
-                  <span className=" text-[32px] font-bold leading-[40px] text-white">
-                    2
-                  </span>
-                  <span className=" font-bold text-[12px] leading-4 text-white">horas</span>
-                </div>
-
-                <div className="ml-6 flex h-[87px] w-[87px] flex-col items-center justify-center rounded-[8px] bg-[#854000]">
-                  <span className=" text-[32px] font-bold leading-[40px] text-white">
-                    2
-                  </span>
-                  <span className=" font-bold text-[12px] leading-4 text-white">minutos</span>
-                </div>
-
-                <div className="ml-6 flex h-[87px] w-[87px] flex-col items-center justify-center rounded-[8px] bg-[#854000]">
-                  <span className=" text-[32px] font-bold leading-[40px] text-white">
-                    2
-                  </span>
-                  <span className=" font-bold text-[12px] leading-4 text-white">segundos</span>
-                </div>
-              </div>
-              <div>
-                <a
-                  href=""
-                  className=" mt-6  flex  h-12 w-[420px] items-center justify-center rounded-[8px] border-white bg-white px-[20px] pb-[7px] pt-[6px] text-[14px] font-bold leading-[17px] text-[#161616]  "
-                >
-                  Saiba Mais
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Main/>
       <Popup/>
     </body>
   );
--- client/src/components/popup.tsx
+++ client/src/components/popup.tsx
@@ -1,16 +1,89 @@
+'use client'
+
 import { ChevronUp } from "lucide-react";
+import { ChevronRight } from "lucide-react";
+import { Search } from "lucide-react";
 import popupImage from '../assets/popup-image.png'
 
 import Image from "next/image";
 
 export default function Popup() {
+
+  function OpenPopup() {
+    var myImage = document.getElementById("Flecha") as HTMLImageElement; // Pegar a variavel da flecha
+    var detalhes = document.getElementById("PopupAberto") as HTMLDivElement; // Pegar a variavel dos detalhes
+    var textoAjuda = document.getElementById('TextoAjuda') as HTMLParagraphElement // Pegar a variavel do texto
+    var BackgroundPopup = document.getElementById('BackgroundPopup') as HTMLDivElement // Pegar a variavel do background
+
+    if (myImage.style.transform == "rotate(180deg)") {
+      myImage.style.transform = "rotate(0deg)";
+    } else {
+      myImage.style.transform = "rotate(180deg)";
+    }
+
+    if (detalhes.style.display == "block") {
+      detalhes.style.display = "none";
+      textoAjuda.innerHTML = 'Central de ajuda'
+      BackgroundPopup.style.background = '#fff'
+      
+    } else {
+      detalhes.style.display = "block";
+      textoAjuda.innerHTML = 'Olá, posso ajudar?'
+      BackgroundPopup.style.background = '#F5F6FA'
+    }
+  }
+  
   return (
-    <div className=" bg-white p-[10px] rounded-lg shadow-lg shadow-black-500/40 flex items-center absolute right-[12.7%] bottom-[3%] w-[223px]">
-        <Image src={popupImage} alt="" className=" w-[34px]"/>
-        <p className=" mx-2 font-bold leading-[17px] text-[14px] text-[#525252] font-inter">Central de Ajuda</p>
-        <div>
-          <ChevronUp className=" h-[1em] w-[1em] text-[#FF7A00]"/>
+    <main>
+      {/* Popup Fechado */}
+      <div className=" bg-white rounded-lg absolute right-[12.7%] bottom-[3%] w-[223px] transition duration-500 ease-in-out" id="BackgroundPopup">
+        <div className="flex items-center p-[10px] ">
+          <Image src={popupImage} alt="" className=" w-[34px]"/>
+          <div className=" flex cursor-pointer" onClick={OpenPopup}>
+            <p className=" mx-2 font-bold leading-[17px] text-[14px] text-[#525252] font-inter" id="TextoAjuda">Central de Ajuda</p>
+            <div>
+              <ChevronUp className=" h-[1em] w-[1em] text-[#FF7A00]" id="Flecha"/>
+            </div>
+          </div>
+      </div>
+
+        {/* Popup Aberto */}
+        <div className="h-[301px] rounded-b-lg w-full bg-[#f5f6fa] p-[10px] hidden" id="PopupAberto">
+          <form action="" className=" w-full relative flex items-center mb-2">
+            <input type="text" name="" id="" placeholder="Pesquisar" className=" w-full border-none outline-none h-8 pl-2 pr-12 text-[15px] rounded-lg font-bold placeholder:text-[12px] "/>
+            <Search className=" rounded-full p-0 right-5 h-5 w-5 bg-transparent absolute"/>
+          </form>
+          <div className=" mb-2">
+            <a href="">
+              <div className="w-full bg-white mb-2 py-4 px-3 relative">
+                <p className=" text-[12.5px] font-normal leading-[15px] mb-4 font-inter text[#6a6c72]">Posso ter uma Conta Conjunta?</p>
+                <div className=" flex gap-1">
+                  <p className=" text-[10px] mb-0 font-normal font-inter leading-[12px]">Escrito por </p>
+                  <strong className=" text-[10px] mb-0 font-normal font-inter leading-[12px] text-[#FF7A00]">
+                    Babi.
+                    </strong>
+                  <ChevronRight className=" h-[17px] absolute right-2 text-[#FF7A00]"/>
+                </div>
+              </div>
+            </a>
+            <a href="" className="mb-2">
+              <div className="w-full bg-white  py-4 px-3 relative">
+              <p className=" text-[12.5px] font-normal leading-[15px] mb-4 font-inter text[#6a6c72]">Como solicito abertura de Conta Digital?</p>
+                <div className=" flex gap-1">
+                  <p className=" text-[10px] mb-0 font-normal font-inter leading-[12px]">Escrito por </p>
+                  <strong className=" text-[10px] mb-0 font-normal font-inter leading-[12px] text-[#FF7A00]">
+                    Babi.
+                    </strong>
+                  <ChevronRight className="h-[17px] absolute right-2 text-[#FF7A00]"/>
+                </div>
+              </div>
+            </a>
+            <div className=" mt-5">
+              <button type="submit" className=" bg-[#FF7A00] font-bold text-[10.5px] leading-[12px] h-8 w-full rounded-lg text-white">Ver todas as respostas</button>
+            </div>
+          </div>
         </div>
       </div>
+    </main>
   )
 }
